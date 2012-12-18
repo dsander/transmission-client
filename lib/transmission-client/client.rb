999b00829b986160c12f63e3694bfdb9a84cd945
Troels Knak-Nielsen <troelskn@gmail.com>
2012-12-18 22:09:14 +0100

    Added yield block to allow callback handlers

--- lib/transmission-client/client.rb
+++ lib/transmission-client/client.rb
@@ -5,38 +5,52 @@ module Transmission
     def on_torrent_stopped(&blk); @on_torrent_stopped = blk; callback_initialized; end
     def on_torrent_started(&blk); @on_torrent_started = blk; callback_initialized; end
     def on_torrent_removed(&blk); @on_torrent_removed = blk; callback_initialized; end
-    
+
     def initialize(host='localhost',port=9091, username = nil, password = nil)
       @connection = Connection.new(host, port, username, password)
       @torrents = nil
     end
-    
+
     def start_all &cb
-      @connection.send('torrent-start')
+      @connection.send('torrent-start') do |resp|
+        yield resp if block_given?
+      end
     end
-    
+
     def start(id)
-      @connection.send('torrent-start', {'ids' => id.class == Array ? id : [id]})
+      @connection.send('torrent-start', {'ids' => id.class == Array ? id : [id]}) do |resp|
+        yield resp if block_given?
+      end
     end
-    
+
     def stop(id)
-      @connection.send('torrent-stop', {'ids' => id.class == Array ? id : [id]})
+      @connection.send('torrent-stop', {'ids' => id.class == Array ? id : [id]}) do |resp|
+        yield resp if block_given?
+      end
     end
-    
+
     def stop_all &cb
-      @connection.send('torrent-stop')
+      @connection.send('torrent-stop') do |resp|
+        yield resp if block_given?
+      end
     end
-    
+
     def remove(id, delete_data = false)
-      @connection.send('torrent-remove', {'ids' => id.class == Array ? id : [id], 'delete-local-data' => delete_data })
+      @connection.send('torrent-remove', {'ids' => id.class == Array ? id : [id], 'delete-local-data' => delete_data }) do |resp|
+        yield resp if block_given?
+      end
     end
-    
+
     def remove_all(delete_data = false)
-      @connection.send('torrent-remove', {'delete-local-data' => delete_data })
+      @connection.send('torrent-remove', {'delete-local-data' => delete_data }) do |resp|
+        yield resp if block_given?
+      end
     end
 
     def add_tracker(id, announce = 'http://retracker.local/announce')
-      @connection.send('torrent-set', {'ids' => id.class == Array ? id : [id], 'trackerAdd' => announce.is_a?(Array) ? announce : [announce] })
+      @connection.send('torrent-set', {'ids' => id.class == Array ? id : [id], 'trackerAdd' => announce.is_a?(Array) ? announce : [announce] }) do |resp|
+        yield resp if block_given?
+      end
     end
 
     def get_trackers(id)
@@ -49,15 +63,21 @@ module Transmission
       if a['filename'].nil? && a['metainfo'].nil?
         raise "You need to provide either a 'filename' or 'metainfo'."
       end
-      @connection.send('torrent-add', a)
+      @connection.send('torrent-add', a) do |resp|
+        yield resp if block_given?
+      end
     end
-    
+
     def add_torrent_by_file(filename)
-      add_torrent({'filename' => filename})
+      add_torrent({'filename' => filename}) do |resp|
+        yield resp if block_given?
+      end
     end
-    
+
     def add_torrent_by_data(data)
-      add_torrent({'metainfo' => data})
+      add_torrent({'metainfo' => data}) do |resp|
+        yield resp if block_given?
+      end
     end
 
 
@@ -71,11 +91,11 @@ module Transmission
         end
       end
     end
-    
+
     def session_stat
       @connection.request('session-stats') { |resp| yield SessionStat.new resp }
     end
-    
+
     #TODO handler for resp['status'] != 'success'
     # options = { 'fields' => ['id'], 'ids' => [1,4,6] }
     def torrents(options = {})
@@ -93,7 +113,7 @@ module Transmission
         end
       }
     end
-    
+
     private
     def callback_initialized
       return if @torrent_poller
@@ -106,12 +126,12 @@ module Transmission
           compare_torrent_status updated_torrents
           @torrents = updated_torrents.dup
         end
-        
-        
+
+
       end
     end
-    
-    def compare_torrent_status updated_torrents
+
+    def compare_torrent_status(updated_torrents)
       return false unless @torrents
       updated_torrents.each_pair do |id, t|
         old = @torrents[t.id] if @torrents[t.id]
@@ -128,9 +148,9 @@ module Transmission
       end
       if @torrents.length > 0 && @on_torrent_removed
         @torrents.values.each do |t|
-          @on_torrent_removed.call t    
+          @on_torrent_removed.call t
         end
-      end  
-    end 
+      end
+    end
   end
 end
