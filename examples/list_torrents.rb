cce1332bf488aedccd7cbcddebffc11bce1cd5fd
Troels Knak-Nielsen <troelskn@gmail.com>
2012-12-18 13:53:03 +0100

    Added a small example to get people started

---
+++ examples/list_torrents.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'transmission-client'
+
+EventMachine.run do
+  t = Transmission::Client.new('127.0.0.1', 9091)
+  t.torrents do |torrents|
+    torrents.each do |tor|
+      puts "#{tor.name} (status: #{tor.status_name}, down: #{tor.rate_download}, up: #{tor.rate_upload}, progress: #{tor.percent_done}, eta: #{tor.eta})"
+    end
+    EventMachine.stop
+  end
+end
